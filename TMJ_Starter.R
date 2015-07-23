50c46c7930c4e0e50c4483aac2cc76d0438143e3
nvraja <mail.nvraja@gmail.com>
2015-07-24 01:42:12 +0530

    With stacked area plot

--- TMJ_Starter.R
+++ TMJ_Starter.R
@@ -112,15 +112,228 @@ GData.MDU.Complete <- GData.MDU[complete.cases(GData.MDU),]
 
 # Gold weight: day/date wise
 GData.MDU.dateQuantity <- aggregate(quantity ~ Date, data=GData.MDU.Complete, FUN=sum, na.rm=TRUE)
-GData.MDU.dateQuantity$Year <- format(GData.MDU.dateQuantity$Date, "%Y")
+#GData.MDU.dateQuantity$Year <- format(GData.MDU.dateQuantity$Date, "%Y")
 
 # To check the number of days in each year for which data is available:
-table(GData.MDU.dateQuantity$Year)
+#table(GData.MDU.dateQuantity$Year)
 
 # ggplot library for plotting:
 require(ggplot2)
 
 # Plot quantity of gold sold per day:
 ggplot(GData.MDU.dateQuantity[GData.MDU.dateQuantity$Year=="2009",], aes(Date, quantity)) + geom_line()
+ggplot(GData.MDU.dateQuantity, aes(Date, quantity)) + geom_line()
+
+# To time series data
+require(zoo)
+GData.MDU.dateQuantity.Zoo <- read.zoo(GData.MDU.dateQuantity)
+#index(GData.MDU.dateQuantity.Zoo) <- GData.MDU.dateQuantityZoo$Date
+
+# Coercing to timeseries
+# GData.MDU.dateQuantity.Ts <- ts(GData.MDU.dateQuantity.Zoo)
+
+# Extensible timeseries
+require(xts)
+
+GData.MDU.dateQuantity.xts <- as.xts(GData.MDU.dateQuantity.Zoo)
+GData.MDU.dateQuantity.Weekly <- apply.weekly(GData.MDU.dateQuantity.xts, sum)
+GData.MDU.dateQuantity.Monthly <- apply.monthly(GData.MDU.dateQuantity.xts, sum)
+
+#GData.MDU.dateQuantity.Weekly <- apply.weekly(GData.MDU.dateQuantity.xts, colMeans) # When weekly mean is required
+
+# Plot 
+plot(GData.MDU.dateQuantity.Weekly, main="Gold weight in grams for MDU - Weekly", xlab="Week", ylab="grams")
+plot(GData.MDU.dateQuantity.Monthly, main="Gold weight in grams for MDU - Monthly", xlab="Month", ylab="grams")
+
+# Seansonal plot
+# epoints <- endpoints(GData.MDU.dateQuantity.xts, on = 'months')             
+# GData.MDU.dateQuantity.Monthly <- period.apply(x = GData.MDU.dateQuantity.xts, INDEX = epoints, FUN = sum)
+
+# seasonplot(GData.MDU.dateQuantity.Monthly, main="Gold weight in grams for MDU", xlab="week", ylab="grams", year.labels=TRUE, year.labels.left=TRUE, col=1:20, pch=19)
+
+## Auto correlation function (Acf):
+# For Monthly data
+Acf(window(GData.MDU.dateQuantity.Monthly,start="2009-01-31", end="2015-03-31-.1"), main="The auto correlation function - Monthly data")
+# For weekly data (note the end date)
+Acf(window(GData.MDU.dateQuantity.Weekly,start="2009-01-31", end="2015-03-29-.1"), main="The auto correlation function - Weekly data")
+
+## Forecasts:
+# Average:
+meanf(GData.MDU.dateQuantity.Weekly,3)
+meanf(GData.MDU.dateQuantity.Monthly,3)
+
+# Naive:
+naive(GData.MDU.dateQuantity.Weekly,3)
+naive(GData.MDU.dateQuantity.Monthly,3)
+
+rwf(GData.MDU.dateQuantity.Weekly,3)
+rwf(GData.MDU.dateQuantity.Monthly,3)
+
+# Seasonal naive:
+snaive(GData.MDU.dateQuantity.Weekly,3)
+snaive(GData.MDU.dateQuantity.Monthly,3)
+
+# Drift:
+rwf(GData.MDU.dateQuantity.Weekly,3, drift=TRUE)
+rwf(GData.MDU.dateQuantity.Monthly,3, drift=TRUE)
+
+# Box-cox:
+lambda <- BoxCox.lambda(GData.MDU.dateQuantity.Weekly)
+plot(BoxCox(GData.MDU.dateQuantity.Weekly,lambda))
+
+# Residual plots:
+# Monthly data
+residual.snaive.monthly <- residuals(snaive(window(GData.MDU.dateQuantity.Monthly,end="2014-12-31")))
+Acf.snaive.monthly <- Acf(residual.snaive.monthly)
+hist(residual.snaive.monthly, nclass="FD", main="Histogram of residuals for monthly data")
+
+# Weekly data
+plot(window(GData.MDU.dateQuantity.Weekly,end="2014-12-28"))
+
+residual.snaive.weekly <- residuals(snaive(window(GData.MDU.dateQuantity.Weekly,end="2014-12-28")))
+Acf.snaive.weekly <- Acf(residual.snaive.weekly)
+hist(residual.snaive.weekly, nclass="FD", main="Histogram of residuals for monthly data")
+
+# Box-pierce test: (similar to combined effect of lag in Acf)
+Box.test(residual.snaive.monthly, lag=10, fitdf=0)
+Box.test(residual.snaive.weekly, lag=10, fitdf=0)
+
+## ==================================
+
+# Unique categories for columns in newdf:
+# Metals:
+nrow(as.data.frame(table(newdf$itm_MType_vc)))
+
+# Item categories:
+nrow(as.data.frame(table(newdf$itm_L2Name_vc)))
+
+# Dataframe ordered items:
+x = as.data.frame(table(GData.MDU.Complete$itm_L2Name_vc))
+
+x <- x[order(-x[,2]),]
+
+## Gold - Madurai
+# Item category (based on frequency):
+GData.MDU.Complete.ItemClass <- GData.MDU.Complete[,c("Date", "itm_L2Name_vc", "quantity")]
+GData.MDU.Complete.ItemClass$itm_L2Name_vc <- as.character(GData.MDU.Complete.ItemClass$itm_L2Name_vc)
+
+GData.MDU.Complete.ItemClass$ItemClass <- ifelse(!(GData.MDU.Complete.ItemClass$itm_L2Name_vc %in% c("PLAIN STUD","CHAIN","RING", "DOLLAR", "COIN", "BANGLE", "STONE STUD", "NECKLACE", "BACK CHAIN", "MALAI", "BRACELET", "THALI", "ROUND", "BESARI")), "OTHERS",GData.MDU.Complete.ItemClass$itm_L2Name_vc)
+
+# Group by month-year
+require(zoo)
+
+GData.MDU.Complete.ItemClass$MonthYear <- as.yearmon(GData.MDU.Complete.ItemClass$Date)
+# GData.MDU.Complete.ItemClass$MonthYear <- as.factor(GData.MDU.Complete.ItemClass$MonthYear)
+
+# Stacked area plot: 
+library(dplyr)
+# Date wise grouped:
+GData.MDU.Complete.ItemClassSummed.Date  <-	GData.MDU.Complete.ItemClass%>%
+						group_by(Date, ItemClass)%>%
+						summarise(quantitySum =sum(quantity, na.rm=TRUE))
+# Month wise grouped:
+GData.MDU.Complete.ItemClassSummed.Month  <-	GData.MDU.Complete.ItemClass%>%
+						group_by(MonthYear, ItemClass)%>%
+						summarise(quantitySum =sum(quantity, na.rm=TRUE))
+
+# Month wise grouped:
+GData.MDU.Complete.ItemClassSummed.Month  <-	GData.MDU.Complete.ItemClass%>%
+						group_by(MonthYear, ItemClass)%>%
+						summarise(quantitySum =sum(quantity, na.rm=TRUE))
+
+# To order by month-year (as such the month-year is character/factor and not time ordered):
+GData.MDU.Complete.ItemClassSummed.Month$MonthYear <- as.Date(GData.MDU.Complete.ItemClassSummed.Month$MonthYear)
+
+library(ggplot2)
+par(mfrow=c(1,2))
+
+ggplot(GData.MDU.Complete.ItemClassSummed.Date, aes(x=Date, y=quantitySum, group=ItemClass, fill=ItemClass)) + geom_area(position="fill")
+
+ggplot(GData.MDU.Complete.ItemClassSummed.Month, aes(x=MonthYear, y=quantitySum, group=ItemClass, fill=ItemClass)) + geom_area(position="fill")
+
+## Considering only the top items:
+# GData.MDU.Complete.ItemClassSummed.Date.filter1 <- filter(GData.MDU.Complete.ItemClassSummed.Date, ItemClass %in% c("PLAIN STUD", "CHAIN", "RING", "DOLLAR", "COIN")) 
+GData.MDU.Complete.ItemClassSummed.Month.filter1 <- filter(GData.MDU.Complete.ItemClassSummed.Month, ItemClass %in% c("PLAIN STUD", "CHAIN")) 
+GData.MDU.Complete.ItemClassSummed.Month.filter2 <- filter(GData.MDU.Complete.ItemClassSummed.Month, ItemClass %in% c("RING", "DOLLAR", "COIN", "BANGLE", "STONE STUD")) 
+
+ItemTrendGoldMdu1 <- ggplot(GData.MDU.Complete.ItemClassSummed.Month.filter1, aes(x=MonthYear, y=quantitySum, group=ItemClass, fill=ItemClass)) + geom_area(position="fill") + labs(x = "Month-Year", y = "Proportion (%)", title="Major gold items and its proportion over time: MADURAI")
+
+ItemTrendGoldMdu <- ggplot(GData.MDU.Complete.ItemClassSummed.Month.filter2, aes(x=MonthYear, y=quantitySum, group=ItemClass, fill=ItemClass)) + geom_area(position="fill") + labs(x = "Month-Year", y = "Proportion (%)", title="Major gold items and its proportion over time: MADURAI")
+
+pdf()
+ItemTrendGoldMdu
+dev.off()
 
 
+### ===============
+# Silver - Madurai:
+SData.MDU <- newdf[newdf$itm_MType_vc=="SILVER" & newdf$StoreCode=="MDU", ]
+SData.MDU.Complete <- SData.MDU[complete.cases(SData.MDU),]
+
+## Items Ordered by frequency:
+# Dataframe ordered items:
+x = as.data.frame(table(SData.MDU.Complete$itm_L2Name_vc))
+
+x <- x[order(-x[,2]),]
+
+# Item category (based on frequency):
+SData.MDU.Complete.ItemClass <- SData.MDU.Complete[,c("Date", "itm_L2Name_vc", "quantity")]
+SData.MDU.Complete.ItemClass$itm_L2Name_vc <- as.character(SData.MDU.Complete.ItemClass$itm_L2Name_vc)
+
+SData.MDU.Complete.ItemClass$ItemClass <- ifelse(!(SData.MDU.Complete.ItemClass$itm_L2Name_vc %in% c("BOMBAY KOLUSU", "KUSHBU", "BOMBAY METTI", "SALEM METTI", "ARADHANA", "JEWEL ACPL", "DIVINITI", "TUMBLER", "JEWEL RATE ITEMS", "SILVER COIN", "ORNAMENT WEIGHT", "KAMATCHI VILAKU", "KUTHIYAM KOLUSU", "BOWL")), "OTHERS", SData.MDU.Complete.ItemClass$itm_L2Name_vc)
+
+# Group by month-year
+require(zoo)
+
+SData.MDU.Complete.ItemClass$MonthYear <- as.yearmon(SData.MDU.Complete.ItemClass$Date)
+
+# Stacked area plot: 
+library(dplyr)
+
+# Month wise grouped:
+SData.MDU.Complete.ItemClassSummed.Month  <- SData.MDU.Complete.ItemClass%>%
+						group_by(MonthYear, ItemClass)%>%
+						summarise(quantitySum =sum(quantity, na.rm=TRUE))
+
+# To order by month-year (as such the month-year is not time ordered):
+SData.MDU.Complete.ItemClassSummed.Month$MonthYear <- as.Date(SData.MDU.Complete.ItemClassSummed.Month$MonthYear)
+
+## Plotting: Silver-Madurai
+library(ggplot2)
+
+ggplot(SData.MDU.Complete.ItemClassSummed.Date, aes(x=Date, y=quantitySum, group=ItemClass, fill=ItemClass)) + geom_area(position="fill")
+
+ggplot(SData.MDU.Complete.ItemClassSummed.Month, aes(x=MonthYear, y=quantitySum, group=ItemClass, fill=ItemClass)) + geom_area(position="fill")
+
+## Considering only the top items:
+# SData.MDU.Complete.ItemClassSummed.Date.filter1 <- filter(SData.MDU.Complete.ItemClassSummed.Date, ItemClass %in% c("PLAIN STUD", "CHAIN", "RING", "DOLLAR", "COIN")) 
+SData.MDU.Complete.ItemClassSummed.Month.filter1 <- filter(SData.MDU.Complete.ItemClassSummed.Month, ItemClass %in% c("PLAIN STUD", "CHAIN")) 
+SData.MDU.Complete.ItemClassSummed.Month.filter2 <- filter(SData.MDU.Complete.ItemClassSummed.Month, ItemClass %in% c("RING", "DOLLAR", "COIN", "BANGLE", "STONE STUD")) 
+
+ItemTrendSilverMdu1 <- ggplot(SData.MDU.Complete.ItemClassSummed.Month.filter1, aes(x=MonthYear, y=quantitySum, group=ItemClass, fill=ItemClass)) + geom_area(position="fill") + labs(x = "Month-Year", y = "Proportion (%)", title="Major Silver items and its proportion over time: MADURAI")
+
+ItemTrendSilverMdu <- ggplot(SData.MDU.Complete.ItemClassSummed.Month.filter2, aes(x=MonthYear, y=quantitySum, group=ItemClass, fill=ItemClass)) + geom_area(position="fill") + labs(x = "Month-Year", y = "Proportion (%)", title="Major Silver items and its proportion over time: MADURAI")
+
+
+
+### ==========================
+library(dplyr)
+
+d1 = data.frame(c1=rep(1:2,each=2), c2=rep(LETTERS[1:2],2), c3=rep(5:6, each=2))
+d2 = data.frame(c1=rep(1:2,each=2), c2=rep(LETTERS[1:2],2), c3=c(1.4,2.7,3.2,7.7))
+d0 <- rbind(d1,d2)
+
+d0%>%
+    group_by(c1,c2)%>%
+    summarise(summation =sum(c3, na.rm=TRUE))
+
+## ====== 
+# Aggregating by month and year
+x <- as.POSIXct(c("2011-02-01", "2011-02-01", "2011-02-01","2012-04-01", "2012-04-01", "2012-05-01"))
+mo <- strftime(x, "%m")
+yr <- strftime(x, "%Y")
+amt <- runif(6)
+dd <- data.frame(mo, yr, amt)
+
+dd.agg <- aggregate(amt ~ mo + yr, dd, FUN = sum)
+dd.agg$date <- as.POSIXct(paste(dd.agg$yr, dd.agg$mo, "01", sep = "-"))
